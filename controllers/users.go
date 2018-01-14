6a9fcc9a735d0eae6a80ac7fdf36e131c0cccf11
eduartua <eduartua@gmail.com>
2018-01-13 22:05:46 -0600

    Adding users.go

--- controllers/users.go
+++ controllers/users.go
@@ -1,2 +1,38 @@
 package controllers
 
+import (
+	"github.com/eduartua/callisto/Galileo/views"
+	"net/http"
+	"fmt"
+)
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("mainLayout", "views/users/new.gohtml"),
+	}
+}
+
+func (u *Users ) New(w http.ResponseWriter, r *http.Request)  {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func (u *Users ) Create(w http.ResponseWriter, r *http.Request)  {
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, "Email is:", form.Email)
+	fmt.Fprintln(w, "Password is:", form.Password)
+}
+
+type Users struct {
+	NewView *views.View
+}
+
+//This is going to change soon
+type SignupForm struct {
+	Email string `schema:"email"`
+	Password string `schema:"password"`
+}
