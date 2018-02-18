1079c792b1cfd77629f30f834a761effcafd35d2
Yohan Graterol <yohangraterol92@gmail.com>
2018-02-17 19:47:17 -0500

    Gorm postgre

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-import "github.com/kataras/iris"
+import (
+	"github.com/kataras/iris"
+	"fmt"
+	"github.com/EthereumCommonwealth/go-callisto/common"
+	"github.com/EthereumCommonwealth/Galileo/models"
+)
 
 func main() {
 	app := iris.New()
+
+	hash := models.Block{Hash: common.StringToHash("asdasdas"), Miner: common.StringToAddress("asdasdasd")}
+
+	fmt.Println(hash.Hash)
+	fmt.Println(hash.Hash.Str())
 
 	app.Run(iris.Addr(":8000"))
 }
--- models/block.go
+++ models/block.go
@@ -2,21 +2,21 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
-	"github.com/EthereumCommonwealth/Galileo/common"
 	"time"
+	"github.com/EthereumCommonwealth/go-callisto/common"
 )
 
 type Block struct {
 	gorm.Model
-	Hash common.Hash	`gorm:"unique_index:hash_block"`
-	ParentHash common.Hash
-	Miner string
+	Hash            common.Hash `gorm:"unique_index:hash_block"`
+	ParentHash      common.Hash
+	Miner           common.Address
 	TransactionRoot common.Hash
-	Difficulty uint
-	Number uint
-	GasLimit uint
-	GasUsed uint
-	Timestamp time.Time
-	ExtraData string
-	Nonce uint
+	Difficulty      uint
+	Number          uint
+	GasLimit        uint
+	GasUsed         uint
+	Timestamp       time.Time
+	ExtraData       string
+	Nonce           uint
 }
